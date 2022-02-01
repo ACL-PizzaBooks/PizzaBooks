559ed9f5e50d1b1ffcc4e017db188c44fae89296
Katie Schrattenholzer <ktschr11@gmail.com>
2022-01-31 17:45:30 -0800

    get by ID route

--- __tests__/author.test.js
+++ __tests__/author.test.js
@@ -42,4 +42,23 @@ describe('author routes', () => {
 
     expect(res.body).toEqual([{...author, id: expect.any(String)}])
   })
+
+  it('should get one author by ID', async() => {
+    const author = await Author.insert({
+      name: 'Dumpling Dockerson',
+      dob: '1995-12-01',
+      pob: 'Steamed Bun Town USA'
+    })
+
+    const res = await request(app).get(`/api/v1/pizzabooks/authors/${author.id}`)
+
+    const expectation = {
+      id: expect.any(String),
+      name: 'Dumpling Dockerson',
+      dob: '12/1/1995',
+      pob: 'Steamed Bun Town USA'
+    }
+
+    expect(res.body).toEqual(expectation)
+  })
 } )
