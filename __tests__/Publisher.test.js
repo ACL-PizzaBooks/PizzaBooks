ac44dd83f1687351f0d1e10c8720bfb47ba58b48
taylor-c-williams <taylor.callan.williams@gmail.com>
2022-01-31 17:17:34 -0800

    publisher get all, get by id, both tests passing

--- __tests__/Publisher.test.js
+++ __tests__/Publisher.test.js
@@ -27,6 +27,35 @@ describe('Publisher Routes', () => {
       ...publisher,
       id: expect.any(String)      
     });
-
   });
+
+  it('gets all publishers', async () => {
+    const realPublisher = await Publisher.insert({ 
+      name: 'Publisher Man',
+      city: 'Des Moines',
+      country: 'USA'
+    });
+
+    const newPublisher = await Publisher.insert({ 
+      name: 'Publisher Werewolf',
+      city: 'Des Moines',
+      country: 'USA'
+    });
+
+    const res = await request(app).get('/api/v1/pizzabooks/publishers');
+    expect(res.body).toEqual([realPublisher, newPublisher]);
+  });
+
+  it ('fetches a single publisher by their ID', async () => {
+    const publisher = await Publisher.insert({ 
+      name: 'Publisher Man',
+      city: 'Des Moines',
+      country: 'USA'
+    });
+    
+    const res = await request(app).get(`/api/v1/pizzabooks/publishers/${publisher.id}`);
+
+    expect(res.body).toEqual(publisher);
+  });
+
 }); 
