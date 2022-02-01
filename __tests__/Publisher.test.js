de581cc02c7735cc12fda746f643a641cdca6c2b
taylor-c-williams <taylor.callan.williams@gmail.com>
2022-01-31 17:12:31 -0800

    publisher insert/create + test passing

--- __tests__/Publisher.test.js
+++ __tests__/Publisher.test.js
@@ -1,7 +1,7 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const app = require('../lib/app');
-// const request = require('supertest');
+const app = require('../lib/app');
+const request = require('supertest');
 const Publisher = require('../lib/models/Publisher');
 
 describe('Publisher Routes', () => {
@@ -19,12 +19,13 @@ describe('Publisher Routes', () => {
       city: 'Des Moines',
       country: 'USA'
     });
-    
-    expect(publisher).toEqual({
-      id: expect.any(String),
-      name: 'Publisher Man',
-      city: 'Des Moines',
-      country: 'USA'
+    const res = await request(app)
+      .post('/api/v1/pizzabooks/publishers')
+      .send(publisher);
+
+    expect(res.body).toEqual({
+      ...publisher,
+      id: expect.any(String)      
     });
 
   });
