a37e9a24a5867f7a9be546fa1ae545fe73dab663
Julius Franklin <jjfranklin88@outlook.com>
2022-01-31 20:40:19 -0500

    all routes passing test

--- __tests__/reviewers.test.js
+++ __tests__/reviewers.test.js
@@ -26,4 +26,49 @@ describe('testing reviewer routes', () => {
     expect(res.body).toEqual({ ...newReviewer, id: expect.any(String) });
   });
 
+  it('should get all reviewers', async () => {
+    const reviewer = await Reviewer.insert({
+      name: 'jane doe',
+      company: 'jane doe and them'
+    });
+
+    const res = await request(app).get('/api/v1/pizzabooks/reviewers');
+
+    expect(res.body).toEqual([reviewer]);
+  });
+
+  it('should get a reviewer by id', async () => {
+    const newReviewer = await Reviewer.insert({
+      name: 'mike',
+      company: 'fake company',
+    });
+
+    const res = await request(app).get(`/api/v1/pizzabooks/reviewers/${newReviewer.id}`);
+
+    expect(res.body).toEqual(newReviewer);
+  });
+
+  it('should update an existing reviewer', async () => {
+
+    const newReviewer = {
+      name: 'jake',
+      company: 'acme inc',
+    };
+
+    const updateReviewer = await Reviewer.insert(newReviewer);
+
+    const res = await request(app).patch(`/api/v1/pizzabooks/reviewers/${updateReviewer.id}`).send({
+      name: 'jacob',
+      company: 'power plant inc',
+    });
+
+    const expectation = {
+      id: expect.any(String),
+      name:'jacob',
+      company: 'power plant inc',
+    };
+
+    expect(res.body).toEqual(expectation);
+    expect(await Reviewer.getReviewerById(updateReviewer.id)).toEqual(expectation);
+  });
 });
