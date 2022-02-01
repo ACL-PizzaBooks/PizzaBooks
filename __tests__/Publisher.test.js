be74baa98f84594e652e0113ae66ecab568bbdc5
taylor-c-williams <taylor.callan.williams@gmail.com>
2022-01-31 16:40:13 -0800

    Publisher insert, Post and test pass

--- __tests__/Publisher.test.js
+++ __tests__/Publisher.test.js
@@ -1,28 +1,31 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-const app = require('../lib/app');
-const request = require('supertest');
+// const app = require('../lib/app');
+// const request = require('supertest');
 const Publisher = require('../lib/models/Publisher');
 
 describe('Publisher Routes', () => {
-    beforeEach(() => {
-      return setup(pool);
-    });
+  beforeEach(() => {
+    return setup(pool);
+  });
   
-    afterAll(() => {
-      pool.end();
-    });
+  afterAll(() => {
+    pool.end();
+  });
 
-    it('creates a Publisher entry', async () => {
-        // const publisher= await Publisher.insert({ name:'Emma Lee Bunton', stagename:'Baby Spice', birthdate:'1/21/1976' 
+  it('creates a Publisher entry', async () => {
+    const publisher = await Publisher.insert({ 
+      name: 'Publisher Man',
+      city: 'Des Moines',
+      country: 'USA'
     });
     
-        expect(true).toBeTruthy;
-        // expect(emma).toEqual({
-        //   id: expect.any(String),
-        //   name: 'Emma Lee Bunton',
-        //   stagename: 'Baby Spice',
-        //   birthdate: '1/21/1976'
-        // });
+    expect(publisher).toEqual({
+      id: expect.any(String),
+      name: 'Publisher Man',
+      city: 'Des Moines',
+      country: 'USA'
+    });
 
-});
+  });
+}); 
