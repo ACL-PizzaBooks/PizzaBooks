856538c863193810e14d37f2933934bab632a19b
taylor-c-williams <taylor.callan.williams@gmail.com>
2022-02-04 09:41:24 -0800

    add jsonb_agg to Book model

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -3,6 +3,7 @@ const setup = require('../data/setup');
 const app = require('../lib/app');
 const request = require('supertest');
 const Book = require('../lib/models/Book');
+const Author = require('../lib/models/Author');
 const Publisher = require('../lib/models/Publisher');
 
 describe('Book Routes', () => {
@@ -14,8 +15,14 @@ describe('Book Routes', () => {
     pool.end();
   });
 
-  it('creates a Book entry', async () => {
+  it('creates a Book entry with a Publisher and an Author', async () => {
 
+    const author = await Author.insert({
+      name: 'Jr. R. Token',
+      dob: '1930-01-02',
+      pob: 'Portland, OR',
+    });
+    
     const publisher = await Publisher.insert({
       name: 'turkey',
       city: 'ashland',
@@ -25,9 +32,9 @@ describe('Book Routes', () => {
     const book = await Book.insert({
       publisher_id:publisher.id,
       released: '2/2/22',
-      title: 'hello book'
+      title: 'hello book', 
+      author_id: author.id
     });
-
 
     const res = await request(app)
       .post('/api/v1/pizzabooks/books')
@@ -39,21 +46,21 @@ describe('Book Routes', () => {
     });
   });
 
-  it('gets all books', async () => {
-    const realPublisher = await Publisher.insert({
-      name: 'Publisher Man',
-      city: 'Des Moines',
-      country: 'USA'
-    });
+  // it('gets all books', async () => {
+  //   const realPublisher = await Publisher.insert({
+  //     name: 'Publisher Man',
+  //     city: 'Des Moines',
+  //     country: 'USA'
+  //   });
 
-    const newPublisher = await Publisher.insert({
-      name: 'Publisher Werewolf',
-      city: 'Des Moines',
-      country: 'USA'
-    });
+  //   const newPublisher = await Publisher.insert({
+  //     name: 'Publisher Werewolf',
+  //     city: 'Des Moines',
+  //     country: 'USA'
+  //   });
 
-    const res = await request(app).get('/api/v1/pizzabooks/publishers');
-    expect(res.body).toEqual([realPublisher, newPublisher]);
-  });
+  //   const res = await request(app).get('/api/v1/pizzabooks/publishers');
+  //   expect(res.body).toEqual([realPublisher, newPublisher]);
+  // });
 
 });
